fa21149c7959514919ab0b7d60ce4416e9f9c0b5
agussantillan01 <agusantillan01@hotmail.com>
2023-05-18 21:19:51 -0300

    Nuevas clases

---
+++ src/negocioImpl/PersonaNegocioImpl.java
@@ -0,0 +1,35 @@
+package negocioImpl;
+
+import java.util.List;
+
+import dao.PersonaDao;
+import daoImpl.PersonaDaoImpl;
+import entidad.Persona;
+
+
+public class PersonaNegocioImpl {
+	PersonaDao pdao = (PersonaDao) new PersonaDaoImpl();
+	
+	public boolean insert(Persona persona) {
+		
+		boolean estado=false;
+		if(persona.getNombre().trim().length()>0 && persona.getDni().trim().length()>0 && persona.getApellido().trim().length()>0)
+		{
+			estado=pdao.insert(persona);
+		}
+		return estado;
+	}
+
+	public boolean delete(Persona persona_a_eliminar) {
+		boolean estado=false;
+		if(persona_a_eliminar.getDni().length()>0 )//Tambi�n se puede preguntar si existe ese ID 
+		{
+			estado=pdao.delete(persona_a_eliminar);
+		}
+		return estado;
+	}
+
+	public List<Persona> readAll() {
+		return pdao.readAll();
+	}
+}
